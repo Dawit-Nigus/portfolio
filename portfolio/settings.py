70323d6de898058730f053faa3b8176cdb880d5a
Dawit Nigus <101523835+Dawit-Nigus@users.noreply.github.com>
2022-07-01 18:02:55 +0300

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -137,6 +137,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage' 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
