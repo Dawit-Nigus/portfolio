bdd9baa3f2a6a044cf8c111edac5844b1a2cbdf7
Dawit Nigus <101523835+Dawit-Nigus@users.noreply.github.com>
2022-07-01 18:00:10 +0300

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -136,7 +136,6 @@ MEDIA_URL = '/images/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
